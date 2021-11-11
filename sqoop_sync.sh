2cb97fcde3c135200d7a34b5a11b10766dfcd186
laerhu <wenreb@outlook.com>
2021-11-11 15:26:29 +0800

    delete hidden temporary files "_SCRATCH*"
    allign indent characters

--- sqoop_sync.sh
+++ sqoop_sync.sh
@@ -1,4 +1,4 @@
-tft() { 
+tft() {
     if [ ${1:8:2} -lt 7 ]
     then d=`date -d "${1:0:8} -1 day" +%Y%m%d`
     else d=${1:0:8}
@@ -8,10 +8,11 @@ tft() {
     }
 # job
 sqp_imp_d() {
-	sqoop eval --connect $conn --username $u --password $p --query "select 1 from dual$rl" || (echo "Error!"; exit 1)
-	impala-shell -q "alter table $hdb.$htb drop if exists partition (date_timekey='$d')" || (echo "Error!"; exit 1)
-	sqoop import --connect $conn --username $u --password $p --query "$sql" --hcatalog-database $hdb --hcatalog-table $htb --hcatalog-home $hchm -m 1 --hcatalog-partition-keys date_timekey --hcatalog-partition-values $d || (echo "Error!"; exit 1)
-	impala-shell -q "refresh $hdb.$htb partition (date_timekey='$d')" || (echo "Error!"; exit 1)
+    sqoop eval --connect $conn --username $u --password $p --query "select 1 from dual$rl" || (echo "Error!"; exit 1)
+    impala-shell -q "alter table $hdb.$htb drop if exists partition (date_timekey='$d')" || (echo "Error!"; exit 1)
+    sqoop import --connect $conn --username $u --password $p --query "$sql" --hcatalog-database $hdb --hcatalog-table $htb --hcatalog-home $hchm -m 1 --hcatalog-partition-keys date_timekey --hcatalog-partition-values $d || (echo "Error!"; exit 1)
+    hdfs dfs -rm -r -f -skipTrash /user/hive/warehouse/$hdb.db/$htb/_SCRATCH*
+    impala-shell -q "refresh $hdb.$htb partition (date_timekey='$d')" || (echo "Error!"; exit 1)
     }
 
 tft $2 
